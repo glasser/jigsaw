d5f6c7a88ace4f7b9c6261d818bd5c64801762a2
David Glasser <glasser@davidglasser.net>
2012-10-17 00:33:15 -0700

    Log in as any user by name.

--- jigsaw.js
+++ jigsaw.js
@@ -1,19 +1,51 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to jigsaw.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+Jigsaw = {};
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.publish("directory", function () {
+    return Meteor.users.find({}, {username: 1});
+  });
+
+  Accounts.registerLoginHandler(function (options) {
+    if (!options.justUsername)
+      return undefined;  // don't handle
+    var user = Meteor.users.findOne({username: options.justUsername});
+    if (user) {
+      var stampedLoginToken = Accounts._generateStampedLoginToken();
+      Meteor.users.update(
+        user._id, {$push: {'services.resume.loginTokens': stampedLoginToken}});
+
+      return {token: stampedLoginToken.token, id: user._id};
+    } else {
+      user = {username: options.justUsername};
+      return Accounts.insertUserDoc({generateLoginToken: true}, user);
+    }
+  });
+} else {
+  Meteor.subscribe("directory");
+
+  Jigsaw.loginAsUser = function (username, callback) {
+    Meteor.apply('login', [{justUsername: username}], {wait: true},
+                 function (error, result) {
+                   if (error || !result) {
+                     error = error || new Error("No result from call to login");
+                     callback && callback(error);
+                     return;
+                   }
+
+                   Accounts._makeClientLoggedIn(result.id, result.token);
+                   callback && callback();
+                 });
+  };
+
+  Template.hello.users = function () {
+    return Meteor.users.find();
+  };
+  Template.hello.events({
+    'click .directory li': function (e) {
+      Jigsaw.loginAsUser(this.username);
+    },
+    'click #logout': function (e) {
+      Meteor.logout();
+    }
   });
 }
