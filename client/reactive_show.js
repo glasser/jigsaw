276f2292ece3183e0de0cd0a007e37cb9c0b0775
David Glasser <glasser@davidglasser.net>
2012-11-21 14:22:08 -0500

    reactive hide/show

---
+++ client/reactive_show.js
@@ -0,0 +1,33 @@
+Handlebars.registerHelper("show", function (options) {
+  if (reactivelyShowing(options.hash.section)) {
+    return options.fn(this) + Template.reactiveHideLink({
+      section: options.hash.section});
+  }
+  return Template.reactiveShowLink({
+    section: options.hash.section,
+    linkText: options.hash.linkText || '[show]'});
+});
+
+// for use with #if
+Handlebars.registerHelper("showing", function (section) {
+  return reactivelyShowing(section);
+});
+
+var reactivelyShow = function (section, show) {
+  Session.set('reactiveShow.' + section, !!show);
+};
+
+var reactivelyShowing = function (section) {
+  return !! Session.get('reactiveShow.' + section);
+};
+
+Template.reactiveShowLink.events({
+  'click a': function (event, template) {
+    reactivelyShow(template.data.section, true);
+  }
+});
+Template.reactiveHideLink.events({
+  'click a': function (event, template) {
+    reactivelyShow(template.data.section, false);
+  }
+});
