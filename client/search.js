fe1abf971c14acec0d6c2640e29d27778ef2af4f
David Glasser <glasser@davidglasser.net>
2012-12-27 15:42:09 -0800

    table on results page. navbar.

--- client/search.js
+++ client/search.js
@@ -5,3 +5,17 @@ Template.searchPage.showing = function () {
 Template.searchPage.search = function () {
   return JigsawRouter.currentSearch();
 };
+
+Template.searchPage.allFamilies = function () {
+  // Want to sort in some consistent order; maybe should actually define a sort
+  // key or something later.
+  return Families.find({}, {sort: ['name']});
+};
+
+// 'this' is a Families object
+Template.searchPage.familyValue = function (puzzleId) {
+  var puzzle = Puzzles.findOne(puzzleId);
+  if (!puzzle)
+    return '';
+  return Meteor._get(puzzle, 'families', this._id);
+};
